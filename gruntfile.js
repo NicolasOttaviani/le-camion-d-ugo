4738ed051f01e55cd29c14737095ca1b052f9eb5
Nicolas Ottaviani <Nicolas.Ottaviani@gmail.com>
2014-05-29 22:57:22 +0200

    Première version du site

--- gruntfile.js
+++ gruntfile.js
@@ -6,16 +6,26 @@ module.exports = function(grunt) {
     'compile-handlebars':{
       allStatic:{
         template: 'src/index.html',
-        templateData: 'src/data/test.json',
+        templateData: 'src/data/data.json',
         output: 'dist/index.html'
       }
-    }
+    },
+    copy: {
+        dist: {
+            cwd: 'src/',
+            src: 'resources/**',
+            dest: 'dist/',
+            expand: true, 
+        }
+    },
+    clean: ['dist']
   });
 
-  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-compile-handlebars');
 
   // Default task(s).
-  grunt.registerTask('default', ['compile-handlebars:allStatic']);
+  grunt.registerTask('default', ['clean', 'copy', 'compile-handlebars:allStatic']);
 
 };
